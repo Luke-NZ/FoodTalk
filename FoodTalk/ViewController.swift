863893997d944eee80ebcaf2bf9716a6cdf6fbcf
李远 <Luke.Li@liyuans-macbook-pro.local>
2016-06-25 21:21:59 +1200

    Initial Commit

---
+++ FoodTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTalk
+//
+//  Created by 李远 on 25/06/16.
+//  Copyright © 2016 Luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
